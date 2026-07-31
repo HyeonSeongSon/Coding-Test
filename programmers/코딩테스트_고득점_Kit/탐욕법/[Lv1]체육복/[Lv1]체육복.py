77a8b15c1f9444e6c41210b2e21e3e81e21aa56a
son <inexorable17@gmail.com>
2026-07-31 23:46:28 +0900

    20260731

---
+++ programmers/코딩테스트_고득점_Kit/탐욕법/[Lv1]체육복/[Lv1]체육복.py
@@ -0,0 +1,17 @@
+def solution(n, lost, reserve):
+
+    lost = set(lost)
+    reserve = set(reserve)
+
+    both = lost & reserve
+
+    lost -= both
+    reserve -= both
+
+    for r in sorted(reserve):
+        if r - 1 in lost:
+            lost.remove(r - 1)
+        elif r + 1 in lost:
+            lost.remove(r + 1)
+
+    return n - len(lost)
